ea97cd4bf294f43ce4597d64e7489e1b84d7df1f
Gangadhar <gangadharkalaga@gmail.com>
2024-11-29 21:20:54 +0530

    task files

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "web_instance" {
-  ami = "ami-022ce6f32988af5fa"
+  ami           = "ami-022ce6f32988af5fa"
   instance_type = "t2.micro"
-  key_name = "DevOps-Key"
-  vpc_security_group_ids = [ "sg-09dd6d33e08227e43" ]
+  key_name      = "DevOps-Key"
+  vpc_security_group_ids = ["sg-09dd6d33e08227e43"]
   tags = {
     Name = "Webserver"
   }
-}
 
-provisioner "local-exec" {
-  command = "echo ${aws_instance.web_instance.public_ip } >> public_ip.txt"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.web_instance.public_ip } >> public_ip.txt"
+  }
 }
